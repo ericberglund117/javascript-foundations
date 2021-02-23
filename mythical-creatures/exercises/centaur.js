119310b983defcf60e7a082081408ca04130b9c5
Eric Berglund <ericberglund117@gmail.com>
2021-02-22 19:34:42 -0700

    Pass centaur class set up test

--- mythical-creatures/exercises/centaur.js
+++ mythical-creatures/exercises/centaur.js
@@ -1,71 +1,116 @@
 class Centaur {
-  constructor(centaur){
+  constructor(centaur) {
     this.name = centaur.name;
-    this.breed = centaur.type;
-    this.cranky = false;
-    this.standing = true;
-    this.task = 0;
-    this.layingDown = false;
-    this.rested = true;
-    }
-    shoot(){
-      this.task++
-      this.cranky = this.task >= 3;
-      // if(this.task >= 3){
-      //   this.cranky = true;
-      // }
-        // if(this.cranky || this.layindDown){
-      // }
-      // if(this.layingDown = true){
-      //   return 'NO!'
-      // }
-      return this.cranky || this.layingDown ? 'NO!' : 'Twang!!!'
-      // return 'Twang!!!'
   }
-    run(){
-      this.task++
-      this.cranky = this.task >= 3;
-      // if(this.task >= 3){
-      //   this.cranky = true
-      // }
-      // if(this.cranky){
-      //   return 'NO!'
-      // }
-      return this.cranky || this.layingDown ? 'NO!' : 'Clop clop clop clop!!!'
-      // return 'Clop clop clop clop!!!'
-    }
-    sleep(){
-      if(this.layingDown){
-        this.cranky = false;
-        this.task = 0;
-      }
-      return this.standing || this.layindDown ? 'NO!' : "ZZZZ"
-      // if(this.standing){
-      //   return 'NO!'
-      // }
-      // if(this.layingDown){
-      //   return 'ZZZZ'
-      // }
-
-    }
-    layDown(){
-      this.standing = false;
-      this.layingDown = true;
-    }
-    standUp(){
-      this.standing = true;
-      this.layingDown = false;
-    }
-    drinkPotion(){
-      if(this.standing && this.task >= 3){
-        this.cranky = false
-        this.rested = false;
-      }
-      if(this.rested){
-        this.cranky = true
-      }
-      return 'Not while I\'m laying down!'
-    }
-  }
+}
 
 module.exports = Centaur;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Centaur {
+//   constructor(centaur){
+//     this.name = centaur.name;
+//     this.breed = centaur.type;
+//     this.cranky = false;
+//     this.standing = true;
+//     this.task = 0;
+//     this.layingDown = false;
+//     this.rested = true;
+//     }
+//     shoot(){
+//       this.task++
+//       this.cranky = this.task >= 3;
+//       // if(this.task >= 3){
+//       //   this.cranky = true;
+//       // }
+//         // if(this.cranky || this.layindDown){
+//       // }
+//       // if(this.layingDown = true){
+//       //   return 'NO!'
+//       // }
+//       return this.cranky || this.layingDown ? 'NO!' : 'Twang!!!'
+//       // return 'Twang!!!'
+//   }
+//     run(){
+//       this.task++
+//       this.cranky = this.task >= 3;
+//       // if(this.task >= 3){
+//       //   this.cranky = true
+//       // }
+//       // if(this.cranky){
+//       //   return 'NO!'
+//       // }
+//       return this.cranky || this.layingDown ? 'NO!' : 'Clop clop clop clop!!!'
+//       // return 'Clop clop clop clop!!!'
+//     }
+//     sleep(){
+//       if(this.layingDown){
+//         this.cranky = false;
+//         this.task = 0;
+//       }
+//       return this.standing || this.layindDown ? 'NO!' : "ZZZZ"
+//       // if(this.standing){
+//       //   return 'NO!'
+//       // }
+//       // if(this.layingDown){
+//       //   return 'ZZZZ'
+//       // }
+
+//     }
+//     layDown(){
+//       this.standing = false;
+//       this.layingDown = true;
+//     }
+//     standUp(){
+//       this.standing = true;
+//       this.layingDown = false;
+//     }
+//     drinkPotion(){
+//       if(this.standing && this.task >= 3){
+//         this.cranky = false
+//         this.rested = false;
+//       }
+//       if(this.rested){
+//         this.cranky = true
+//       }
+//       return 'Not while I\'m laying down!'
+//     }
+//   }
+
+// module.exports = Centaur;
