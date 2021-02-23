1eeb2106f0921288f6baeffb290178e3380dfb22
Eric Berglund <ericberglund117@gmail.com>
2021-02-23 12:13:49 -0700

    Pass Hobbit set up test

--- mythical-creatures/exercises/hobbit.js
+++ mythical-creatures/exercises/hobbit.js
@@ -1,23 +1,34 @@
 class Hobbit {
-  constructor(name, age){
-    this.name = name;
-    this.age = age || 0;
-    this.adult = false;
-    this.old = false;
-    this.hasRing = false;
-    if(this.name === 'Frodo'){
-      this.hasRing = true;
-    }
+  constructor(hobbit) {
+    this.name = hobbit.name
   }
- celebrateBirthday(){
-   this.age++
-   if(this.age > 32){
-     this.adult = true
-   }
-   if (this.age >= 101){
-     this.old = true;
-   }
- }
 }
 
-module.exports = Hobbit
+module.exports = Hobbit;
+
+
+
+
+// class Hobbit {
+//   constructor(name, age){
+//     this.name = name;
+//     this.age = age || 0;
+//     this.adult = false;
+//     this.old = false;
+//     this.hasRing = false;
+//     if(this.name === 'Frodo'){
+//       this.hasRing = true;
+//     }
+//   }
+//  celebrateBirthday(){
+//    this.age++
+//    if(this.age > 32){
+//      this.adult = true
+//    }
+//    if (this.age >= 101){
+//      this.old = true;
+//    }
+//  }
+// }
+
+// module.exports = Hobbit
