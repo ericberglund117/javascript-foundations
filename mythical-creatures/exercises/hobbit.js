39bd072e6a999cd368e19579548ae14c73513ce1
Eric Berglund <ericberglund117@gmail.com>
2021-02-23 12:20:22 -0700

    Pass Hobbit tests

--- mythical-creatures/exercises/hobbit.js
+++ mythical-creatures/exercises/hobbit.js
@@ -1,6 +1,17 @@
 class Hobbit {
-  constructor(hobbit) {
-    this.name = hobbit.name
+  constructor(name) {
+    this.name = name;
+    this.age = 0;
+    this.adult = false;
+    this.old = false;
+    this.hasRing = false;
+    this.name === 'Frodo' ? this.hasRing = true : this.hasRing = false;
+  }
+  
+  celebrateBirthday = () => {
+    this.age++
+    this.age <= 32 ? this.adult = false : this.adult = true;
+    this.age >= 101 ? this.old = true : this.old = false;
   }
 }
 
