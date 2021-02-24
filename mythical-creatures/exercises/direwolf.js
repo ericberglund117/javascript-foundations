1da5eaacc5a9cd8f4289e2d87d7813c8d4e08813
Eric Berglund <ericberglund117@gmail.com>
2021-02-23 17:20:28 -0700

    Pass Direwolf tests

--- mythical-creatures/exercises/direwolf.js
+++ mythical-creatures/exercises/direwolf.js
@@ -1,7 +1,27 @@
 class Direwolf {
-    constructor() {
-
+    constructor(name, home, size) {
+        this.name = name;
+        this.home = home || 'Beyond the Wall';
+        this.size = size || 'Massive';
+        this.starksToProtect = [];
+        this.huntsWhiteWalkers = true;
     }
-}
+
+    protect = (stark) => {
+        stark.safe = true;
+        this.huntsWhiteWalkers = false;
+        if (this.home === stark.location && this.starksToProtect.length < 2) {
+            this.starksToProtect.push(stark)
+        } else {
+            return this.starksToProtect
+        }  
+    };
+
+    leave = (stark) => {
+        stark.safe = false;
+       return this.starksToProtect.splice(stark, 1)
+    }
+
+};
 
 module.exports = Direwolf;
